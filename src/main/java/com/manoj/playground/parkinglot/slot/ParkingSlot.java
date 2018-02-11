d03c1480b3ed19698442f13940f4a820b9b1c119
Manoj Bhat <Manoj.Bhat@ig.com>
2018-02-11 15:41:08 +0530

    Sets cars into slots

--- src/main/java/com/manoj/playground/parkinglot/slot/ParkingSlot.java
+++ src/main/java/com/manoj/playground/parkinglot/slot/ParkingSlot.java
@@ -1,9 +1,14 @@
 package com.manoj.playground.parkinglot.slot;
 
+import com.manoj.playground.parkinglot.car.Car;
+
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
 public class ParkingSlot {
+
    private final String slotId;
+
+   private Car car;
 
    public ParkingSlot(String slotId) {
       if (isBlank(slotId)) {
@@ -12,7 +17,19 @@ public class ParkingSlot {
       this.slotId = slotId;
    }
 
+   public boolean isEmpty() {
+      return this.car == null;
+   }
+
    public String getSlotId() {
       return slotId;
+   }
+
+   public Car getCar() {
+      return car;
+   }
+
+   public void setCar(Car car) {
+      this.car = car;
    }
 }
--- src/test/java/com/manoj/playground/parkinglot/slot/ParkingSlotTest.java
+++ src/test/java/com/manoj/playground/parkinglot/slot/ParkingSlotTest.java
@@ -1,7 +1,10 @@
 package com.manoj.playground.parkinglot.slot;
 
+import com.manoj.playground.parkinglot.car.Car;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Java6Assertions.assertThatThrownBy;
 
@@ -18,5 +21,20 @@ class ParkingSlotTest {
       assertThatThrownBy(() -> new ParkingSlot("  "))
          .isInstanceOf(IllegalArgumentException.class)
          .withFailMessage("Invalid input. Slot Id mandatory for creating a slot!");
+   }
+
+   @Test
+   void addACarToAParkingSlot() {
+      Car redCar = new Car("regNumber", "red");
+      ParkingSlot parkingSlot = new ParkingSlot("1");
+      parkingSlot.setCar(redCar);
+      assertThat(parkingSlot.getCar()).isEqualTo(redCar);
+      assertThat(parkingSlot.isEmpty()).isEqualTo(FALSE);
+   }
+
+   @Test
+   void shouldBeAbleToCheckIfSlotIsEmpty() {
+      ParkingSlot parkingSlot = new ParkingSlot("2");
+      assertThat(parkingSlot.isEmpty()).isEqualTo(TRUE);
    }
 }
